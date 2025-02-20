0d8f02d2b8f6891086fb64c7f80aba469d31f661
deadlovelll <playboicartina@gmail.com>
2025-02-20 23:35:36 +0300

    fixed fastapi and celery compose

--- house_zhkh_core/project/celery.py
+++ house_zhkh_core/project/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.settings')
 
-app = Celery('house_zhkh_core')
+app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- house_zhkh_core/project/settings/settings.py
+++ house_zhkh_core/project/settings/settings.py
@@ -63,13 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "project.wsgi.application"
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',  # Database file stored in the project root
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DATABASE_ENGINE'),
