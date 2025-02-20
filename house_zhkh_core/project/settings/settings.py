2798d86e9c74f53e85c4c7ac3d3dc82ef42225d0
deadlovelll <playboicartina@gmail.com>
2025-02-21 01:06:38 +0300

    docker compose done

--- house_zhkh_core/project/settings/settings.py
+++ house_zhkh_core/project/settings/settings.py
@@ -69,9 +69,9 @@ DATABASES = {
         'OPTIONS': {
             'options': os.getenv('DB_OPTIONS')
         },
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     },
